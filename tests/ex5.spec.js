f9a880648a7986710f244f7758b3d79fee98e90a
Jaisa2001 <jaisaicharan2001@gmail.com>
2024-08-14 15:14:12 +0530

    errors fixed

--- tests/ex5.spec.js
+++
@@ -1,9 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('cloud that', async ({page}) => {
-    await page.goto("https://login.salesforce.com/?locale=in");
-    await page.getByPlaceholder("Username or email").fill('xyz@gmail.com');
-    await page.getByPlaceholder("Password").fill("123");
-   
-    await page.locator("#loginbtn").click()
-})
