f1040db14db87d00a34cf641215fc068d69b59b0
Jaisa2001 <jaisaicharan2001@gmail.com>
2024-08-18 07:42:10 +0530

    error

--- tests/testpage2.spec.js
+++ tests/testpage2.spec.js
@@ -1,11 +1,16 @@
 const{test,expect}=require('@playwright/test');
 
-test("loginpage", async ({page}) => {
+test("loginpage1", async ({page}) => {
     await page.goto("https://demowebshop.tricentis.com/");
  
     await page.waitForTimeout(2000);
  
     await page.locator(".ico-login").click();
+
+});
+
+test("loginpage2", async ({page}) => {
+    await page.goto("https://demowebshop.tricentis.com/login");
  
     await expect(page).toHaveURL("https://demowebshop.tricentis.com/login");
  
@@ -14,6 +19,32 @@ test("loginpage", async ({page}) => {
     const title=await page.getByText("Welcome, Please Sign In!");
  
     await expect(title).toBeVisible;
+
+});
+
+test("loginpage3", async ({page}) => {
+    await page.goto("https://demowebshop.tricentis.com/login");
+
+    const locator = page.locator(".topic-html-content-body");
+
+    await expect(locator).toHaveText("Put your login / registration information here. You can edit this in the admin site.");
+
+});
+
+test("loginpage4", async ({page}) => {
+    await page.goto("https://demowebshop.tricentis.com/");
+
+    const value = { name: "Returning Customer" };
+
+    expect(value).toBeTruthy();
+
+
+});
+
+
+
+test("loginpage5", async ({page}) => {
+    await page.goto("https://demowebshop.tricentis.com/login");
  
     await page.fill("#Email","allem12@gmail.com");
  
